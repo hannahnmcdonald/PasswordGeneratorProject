1d4feac58ab505f263c6b3d4ee7b13a740f88efd
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-07 21:56:00 -0400

    fixed .isNAN formatting issue per MDN Web Docs Article

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 
 // PROMPTS for password questions:
-function getPasswordCharacters() {
+function.getCharacterOptions('generate').addEventListener("click") {
   var length = parseInt(
     prompt('How many characters would you like your password to be?')
     );
-  if (length.isNAN) {
+    // Fixed .isNAN format per MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
+  if (number.isNAN(length)) {
+    alert("Password length must be numerical");
+    return null;
+  }if(length<8 || length>128) {
     alert("Password length must be between 8 and 128 characters.");
     return null;
-  }if(length<8) {
-    alert("Password length must be between 8 and 128 characters.");
-    return null;
-  }if(length>128) {}
-    alert('"Password length must be between 8 and 128 characters."');
-    return null;
+  }
+
+  // IF none are chosen, password generator has no options to pull from, and program ends
+  if (!includelower && !includenumbers && !includeupper && !includespecial) {
+  alert('You must choose atleast one of the options!');
+  return null;
+  }else{
+  // CONFIRM messages asking for additional options
+  var includespecial = confirm('Would you like to include special characters?');
+  var includeupper = confirm('Would you like to include uppercase characters?');
+  var includelower = confirm('Would you like to include lowercase characters?');
+  var includenumbers= confirm('Would you like to include numbers?');
+  }
+  
+  // CHARACTER CODES using ASCII Codes
+  var uppercasecharcodes = arrayFromLowToHigh(65, 90)
+  var lowercasecharcodes = arrayFromLowToHigh(97, 122)
+  var numbercharcodes = arrayFromLowToHigh(48, 57)
+  var specialcharcodes = arrayFromLowToHigh(33, 47).concat(
+    arrayFromLowToHigh(58, 64)
+    ).concat(arrayFromLowToHigh(91, 96)
+    ).concat(arrayFromLowToHigh(123, 126)
+    )
 }
 
-var includespecial
-var includeupper
-var includelower
-var includenumbers
-
-
-
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+button.addEventListener("click"){
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// var passwordCharacterOptions= [];
+// if (CharacterOptions.includespecial) {
+//   passwordCharacterOptions = passwordCharacterOptions.concat(specialcharcodes);
+// }if (CharacterOptions.includenumbers) {
+//   passwordCharacterOptions = passwordCharacterOptions.concat(numbercharcodes);
+// }if (CharacterOptions.includelower) {
+//   passwordCharacterOptions = passwordCharacterOptions.concat(lowercasecharcodes);
+// }if (CharacterOptions.includeupper) {
+//   passwordCharacterOptions = passwordCharacterOptions.concat(uppercasecharcodes);
+// }
+
+
+
+// var generateBtn = document.querySelector("#generate");
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
