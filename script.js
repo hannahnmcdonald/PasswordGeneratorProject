0710a96ccf7a1fe2b2c68aa0601f01ff24f04844
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-08 17:08:53 -0400

    added function from arrayFromLowToHigh which was accidentally left out earlier. Still need to fix the .isNaN option and the loop

--- script.js
+++ script.js
@@ -13,35 +13,35 @@ function generatePassword() {
 
   let passwordlength = prompt("Select your desired password length");
 
+  if(Number.isNaN(passwordlength)) {
+    // IF input for password length is not numerical, user gets an alert
+  alert("Password length must be numerical");
+  return null;
+  }
+
   if(passwordlength<8 || passwordlength>128) {
     // IF input for password length is not long enough, user gets an alert
   alert("Password length must be between 8 and 128 characters.");
   return null;
   }
-  // if(Number.isNan(passwordlength)) {
-  //   // IF input for password length is not numerical, user gets an alert
-  // alert("Password length must be numerical");
-  // return null;
-  // }
+
     var includespecial = confirm('Would you like to include special characters?');
     var includeupper = confirm('Would you like to include uppercase characters?');
     var includelower = confirm('Would you like to include lowercase characters?');
     var includenumbers= confirm('Would you like to include numbers?');
 
     // CHARACTER CODES using ASCII Codes
-    const uppercasecharcodes = arrayFromLowToHigh(65, 90)
-    const lowercasecharcodes = arrayFromLowToHigh(97, 122)
-    const numbercharcodes = arrayFromLowToHigh(48, 57)
-    const specialcharcodes = arrayFromLowToHigh(33, 47).concat(
+    var uppercasecharcodes = arrayFromLowToHigh(65, 90);
+    var lowercasecharcodes = arrayFromLowToHigh(97, 122);
+    var numbercharcodes = arrayFromLowToHigh(48, 57);
+    var specialcharcodes = arrayFromLowToHigh(33, 47).concat(
       arrayFromLowToHigh(58, 64)
       ).concat(
         arrayFromLowToHigh(91, 96)
       ).concat(
         arrayFromLowToHigh(123, 126)
-      )
+      );
 
-      // console.log(lowercasecarcodes);
-      
     // IF no options are selected, user gets an error alert
     if (!includelower && !includenumbers && !includeupper && !includespecial) {
     alert('You must choose atleast one of the options!');
@@ -59,8 +59,16 @@ function generatePassword() {
   }
   for (var i = 0; i <passwordlength; i++) {
     passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    passwordCharacters.push(String.fromCharCode())
   }
   return password;
 }
 generateBtn.addEventListener("click",writePassword);
+
+// FUNCTION for above arrayFromLowToHigh
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
