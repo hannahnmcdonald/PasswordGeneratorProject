789af18311adb9f63b0105cf17bf4d39ac3029d7
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-08 16:08:50 -0400

    routine commit- prompts working up until array for ascii codes, will continue to evaluate

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 var generateBtn = document.querySelector("#generate");
 
-// CHARACTER CODES using ASCII Codes
-var uppercasecharcodes = arrayFromLowToHigh(65, 90)
-var lowercasecharcodes = arrayFromLowToHigh(97, 122)
-var numbercharcodes = arrayFromLowToHigh(48, 57)
-var specialcharcodes = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-  ).concat(arrayFromLowToHigh(91, 96)
-  ).concat(arrayFromLowToHigh(123, 126)
-  )
-var passwordlength = "";
-
-
-function displayPassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordCharacters = document.querySelector('#password');
 
@@ -20,7 +8,7 @@ function displayPassword() {
 }
 
 function generatePassword() {
-  userPassword = "";
+  password = "";
   passwordCharacters = "";
 
   let passwordlength = prompt("Select your desired password length");
@@ -30,16 +18,30 @@ function generatePassword() {
   alert("Password length must be between 8 and 128 characters.");
   return null;
   }
-  if(Number.isNan(passwordlength)) {
-    // IF input for password length is not numerical, user gets an alert
-  alert("Password length must be numerical");
-  return null;
-  }
+  // if(Number.isNan(passwordlength)) {
+  //   // IF input for password length is not numerical, user gets an alert
+  // alert("Password length must be numerical");
+  // return null;
+  // }
     var includespecial = confirm('Would you like to include special characters?');
     var includeupper = confirm('Would you like to include uppercase characters?');
     var includelower = confirm('Would you like to include lowercase characters?');
     var includenumbers= confirm('Would you like to include numbers?');
 
+    // CHARACTER CODES using ASCII Codes
+    const uppercasecharcodes = arrayFromLowToHigh(65, 90)
+    const lowercasecharcodes = arrayFromLowToHigh(97, 122)
+    const numbercharcodes = arrayFromLowToHigh(48, 57)
+    const specialcharcodes = arrayFromLowToHigh(33, 47).concat(
+      arrayFromLowToHigh(58, 64)
+      ).concat(
+        arrayFromLowToHigh(91, 96)
+      ).concat(
+        arrayFromLowToHigh(123, 126)
+      )
+
+      // console.log(lowercasecarcodes);
+      
     // IF no options are selected, user gets an error alert
     if (!includelower && !includenumbers && !includeupper && !includespecial) {
     alert('You must choose atleast one of the options!');
@@ -47,15 +49,18 @@ function generatePassword() {
   }
     // MERGE section where all options are compiled
   if (includespecial) {
-    passwordCharacters += lowercasecharcodes;
+    passwordCharacters += specialcharcodes;
   }if (includeupper) {
     passwordCharacters += uppercasecharcodes;
   }if (includelower) {
     passwordCharacters += lowercasecharcodes;
   }if (includenumbers) {
     passwordCharacters += numbercharcodes;
-  }for (var i = 0; i <passwordlength; i++) {
-    userPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-  }return userPassword;
+  }
+  for (var i = 0; i <passwordlength; i++) {
+    passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    passwordCharacters.push(String.fromCharCode())
+  }
+  return password;
 }
-generateBtn.addEventListener("click",displayPassword);
+generateBtn.addEventListener("click",writePassword);
