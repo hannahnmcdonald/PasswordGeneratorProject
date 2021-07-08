f09b950a34d6903a7e8eee8fc42b554079c1d763
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-07 20:39:33 -0400

    declaring variables in javascript + beginning dialogue/alert/prompt/messages

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// Assignment Code
+
+// PROMPTS for password questions:
+function getPasswordCharacters() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to be?')
+    );
+  if (length.isNAN) {
+    alert("Password length must be between 8 and 128 characters.");
+    return null;
+  }if(length<8) {
+    alert("Password length must be between 8 and 128 characters.");
+    return null;
+  }if(length>128) {}
+    alert('"Password length must be between 8 and 128 characters."');
+    return null;
+}
+
+var includespecial
+var includeupper
+var includelower
+var includenumbers
+
+
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
