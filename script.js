643da58a978c34995392ccf260e4c6e1f6955553
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-11 15:13:35 -0400

    added notes to JS code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var generateBtn = document.querySelector("#generate");
-
+// NOTE: Below Input will write the generated password into the #password input area
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
@@ -8,13 +8,14 @@ function writePassword() {
 }
 
 function generatePassword() {
+  // NOTE: Below variables are set to be a string and an array, respectively. The resulting array that is passwordCharacters is turned into a string in the function in lines 67-74
   userpassword = "";
   passwordCharacters = [];
 
   let passwordlength = prompt("Select your desired password length");
 
   if (passwordlength < 8 || passwordlength > 128 || isNaN(passwordlength)) {
-    // IF input for password length is not long enough or is not numerical, user gets an alert
+  // NOTE: input for password length is not long enough or is not numerical, user gets an alert
   alert("Password length must be between 8 and 128 characters and numerical.");
   return null;
   }
@@ -25,7 +26,7 @@ function generatePassword() {
     var includelower = confirm('Would you like to include lowercase characters?');
     var includenumbers= confirm('Would you like to include numbers?');
 
-  // CHARACTER CODES using ASCII Codes
+  // NOTE: CHARACTER CODES using ASCII Codes
 
     var uppercasecharcodes = arrayFromLowToHigh(65, 90);
     var lowercasecharcodes = arrayFromLowToHigh(97, 122);
@@ -38,12 +39,12 @@ function generatePassword() {
         arrayFromLowToHigh(123, 126)
       );
 
-    // IF no options are selected, user gets an error alert
+  // NOTE: IF no options are selected, user gets an error alert
     if (!includelower && !includenumbers && !includeupper && !includespecial) {
     alert('You must choose atleast one of the options!');
     return null;
   }
-    // MERGE section where all options are compiled
+  // MERGE section where all options are compiled
   if (includespecial) {
     passwordCharacters= passwordCharacters.concat(specialcharcodes);
   }if (includeupper) {
@@ -63,7 +64,7 @@ function generatePassword() {
 }
 generateBtn.addEventListener("click",writePassword);
 
-// FUNCTION for above arrayFromLowToHigh
+// NOTE: FUNCTION for above arrayFromLowToHigh AND converts the resulting array into a string
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
